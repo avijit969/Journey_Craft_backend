cefd212cf02771de8bf95c1fec7fed216105b50a
Avijit <abhijitpradhan909@gmail.com>
2024-05-10 11:11:29 +0530

    add CROS_ORIGIN

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser"
 const app = express()
 
 app.use(cors({
-    origin: "http://localhost:5173"||process.env.CORS_ORIGIN,
+    origin: process.env.CORS_ORIGIN||"http://localhost:5173",
     credentials: true
 }))
 app.use((req, res, next) => {
